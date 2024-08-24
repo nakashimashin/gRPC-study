54684492d87fc54b24af9cbe9a840a8e95771beb
nakashimashin <nakashimashin0517@gmail.com>
2024-08-24 21:58:43 +0900

    双方向ストリーミング(クライアントサイド)

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -43,7 +43,8 @@ func main() {
 		fmt.Println("1: send Request")
 		fmt.Println("2: HelloServerStream")
 		fmt.Println("3: HelloClientStream")
-		fmt.Println("4: exit")
+		fmt.Println("4: HelloBiStream")
+		fmt.Println("5: exit")
 		fmt.Println("please enter >")
 
 		scanner.Scan()
@@ -58,6 +59,8 @@ func main() {
 		case "3":
 			HelloClientStream()
 		case "4":
+			HelloBiStream()
+		case "5":
 			fmt.Println("bye.")
 			goto M
 		}
@@ -135,5 +138,51 @@ func HelloClientStream() {
 		fmt.Println(err)
 	} else {
 		fmt.Println(res.GetMessage())
+	}
+}
+
+func HelloBiStream() {
+	stream, err := client.HelloBiStream(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	sendNum := 5
+	fmt.Printf("Please enter %d names.\n", sendNum)
+
+	var sendEnd, recvEnd bool
+	sendCount := 0
+	for !(sendEnd && recvEnd) {
+		if !sendEnd {
+			scanner.Scan()
+			name := scanner.Text()
+
+			sendCount++
+			if err := stream.Send(&hellopb.HelloRequest{
+				Name: name,
+			}); err != nil {
+				fmt.Println(err)
+				sendEnd = true
+			}
+
+			if sendCount == sendNum {
+				sendEnd = true
+				if err := stream.CloseSend(); err != nil {
+					fmt.Println(err)
+				}
+			}
+		}
+
+		if !recvEnd {
+			if res, err := stream.Recv(); err != nil {
+				if errors.Is(err, io.EOF) {
+					fmt.Println(err)
+				}
+				recvEnd = true
+			} else {
+				fmt.Println(res.GetMessage())
+			}
+		}
 	}
 }
